75a5fb881f609b9fa4d8b1b58217376bdea9e63f
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2026-04-02 02:40:59 +0200

    Update build process and assembly merging setup

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890")]
-[assembly: AssemblyVersion("1.2026.4.1")]
-[assembly: AssemblyFileVersion("1.2026.4.1")]
+[assembly: AssemblyVersion("1.2026.4.2")]
+[assembly: AssemblyFileVersion("1.2026.4.2")]
